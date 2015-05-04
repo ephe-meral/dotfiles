641dd01b5774e3da9d16619fd9de2f6316697902
ephe-meral <j.appel.online@gmail.com>
2015-05-04 14:14:55 +0200

    screen lock script adapted

--- i3/.i3/i3lock.sh
+++ i3/.i3/i3lock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 2x2 /tmp/screen_locked2.png
-i3lock -i /tmp/screen_locked2.png
+scrot /tmp/screen_locked1.png
+convert -resize 10% /tmp/screen_locked1.png /tmp/screen_locked2.png
+convert -resize 1000% /tmp/screen_locked2.png /tmp/screen_locked3.png
+#convert /tmp/screen_locked.png -blur 2x2 /tmp/screen_locked2.png
+i3lock -i /tmp/screen_locked3.png
 
