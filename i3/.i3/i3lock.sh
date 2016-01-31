825c6b53e965a9976bd2fc456e472c72c002d8a2
ephe-meral <j.appel.online@gmail.com>
2016-01-31 14:45:25 +0100

    cleanup rust.vim

--- i3/.i3/i3lock.sh
+++ i3/.i3/i3lock.sh
@@ -4,4 +4,3 @@ convert -resize 10% /tmp/screen_locked1.png /tmp/screen_locked2.png
 convert -resize 1000% /tmp/screen_locked2.png /tmp/screen_locked3.png
 #convert /tmp/screen_locked.png -blur 2x2 /tmp/screen_locked2.png
 i3lock -i /tmp/screen_locked3.png
-
